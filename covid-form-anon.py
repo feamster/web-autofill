85654d8ab97d576d3779e58c480f0456e0375adc
Nick Feamster <feamster@uchicago.edu>
2021-02-22 05:29:56 -0600

    edit auto-fill. more teachers in list now.

--- covid-form-anon.py
+++ covid-form-anon.py
@@ -24,21 +24,21 @@ def site_login(driver):
     driver.find_element_by_xpath("/html/body/div/div[2]/form/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div/div[1]/input").send_keys('Last')
 
     # Symptom Checkbox
-    driver.find_element_by_xpath("/html/body/div/div[2]/form/div[2]/div/div[2]/div[6]/div/div/div[2]/div[1]/div[13]/label/div/div[1]/div[2]").click()
+    driver.find_element_by_xpath("/html/body/div/div[2]/form/div[2]/div/div[2]/div[6]/div/div/div[2]/div[1]/div[14]/label/div/div[1]/div[2]").click()
 
     # Select Dropdown
     driver.find_element_by_xpath("/html/body/div/div[2]/form/div[2]/div/div[2]/div[5]/div/div/div[2]/div/div[1]/div[2]").click()
 
-    # Use Down Arrow Keys to Select 7th Option (Teacher)
+    # Use Down Arrow Keys to Select 12th Option (Teacher)
     time.sleep(0.5)
-    for i in range(0,7):
+    for i in range(0,12):
         webdriver.ActionChains(driver).send_keys(Keys.DOWN).perform()
         time.sleep(0.2)
     webdriver.ActionChains(driver).send_keys(Keys.SPACE).perform()
 
     # Submit the Form
     # Tab to the Bottom so we can find the Submit button
-    for i in range(0,15):
+    for i in range(0,16):
         time.sleep(0.3)
         webdriver.ActionChains(driver).send_keys(Keys.TAB).perform()
     for i in range(0,7):
